18e98f52591cbe5667b4a39f2d3c607bfdb6962d
lingyin <dmzlingyin@163.com>
2022-04-19 21:03:03 +0800

    add insertion_sort

--- Golang/sorts/insertion_sort/code.go
+++ Golang/sorts/insertion_sort/code.go
@@ -1,1 +1,16 @@
 package insertion_sort
+
+import "fmt"
+
+func sort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		cur := nums[i]
+		pre := i - 1
+		for pre >= 0 && cur < nums[pre] {
+			nums[pre+1] = nums[pre]
+			pre--
+		}
+		nums[pre+1] = cur
+	}
+	fmt.Println(nums)
+}
--- Golang/sorts/insertion_sort/code_test.go
+++ Golang/sorts/insertion_sort/code_test.go
@@ -1,1 +1,12 @@
 package insertion_sort
+
+import "testing"
+
+func TestSort(t *testing.T) {
+	nums0 := []int{4, 3, 5, 6, 3, 1, 7, 2, 3, 4, 9}
+	nums1 := []int{4, 1, 7, 2, 3, 4, 9}
+	nums2 := []int{9, -1, -999}
+	sort(nums0)
+	sort(nums1)
+	sort(nums2)
+}
